2cfbb9a699a9d84471c26fe4d6eed1a488606e00
brianlash <brian@ammohawk.com>
2017-08-16 17:51:13 -0400

    Add files via upload

---
+++ p20_factorialDigitSum.py
@@ -0,0 +1,15 @@
+# Project Euler problem at https://projecteuler.net/problem=20
+#
+factorial = 100
+
+for x in reversed(range(1,factorial)):
+    factorial *= x
+
+new = list(''.join(str(factorial)))
+
+factorial_sum = 0
+
+for x in new:
+    factorial_sum += int(x)
+
+print(factorial_sum)
