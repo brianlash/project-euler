a701362f2fef15d1053f51df41634390a34df9fa
brianlash <brian@ammohawk.com>
2017-08-16 17:54:04 -0400

    Update p10_sumOfPrimes.py

--- p10_sumOfPrimes.py
+++ p10_sumOfPrimes.py
@@ -1,12 +1,4 @@
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
-# Find the sum of all the primes below two million.
-
-
-# NOTE: This is a brute force solution that treats every odd
-# number as a potential prime candidate. It finds a solution,
-# however it is computationally expensive and requires >10 min
-# on a 2.7GHz machine. There are faster methods.
+# Project Euler problem at https://projecteuler.net/problem=10
 
 def findNthPrime(a):
 
@@ -31,5 +23,5 @@ def findNthPrime(a):
 
     print(sum(primes))
 
-a = 2000000
+a = int(input('Which prime? '))
 findNthPrime(a)
